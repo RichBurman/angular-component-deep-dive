d4998887138073584bf86b3354dced33dd913549
Richard Burman <richard.burman@harperjames.co.uk>
2025-01-07 11:50:58 +0000

    refactor: introduce dashboard-item component for better structure and reusability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServiceStatusComponent } from "./dashboard/service-status/service-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServiceStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServiceStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
   
