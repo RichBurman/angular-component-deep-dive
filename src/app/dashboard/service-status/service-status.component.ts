754d7582451d4a419a7f9088f178cda72ea7546b
Richard Burman <richard.burman@harperjames.co.uk>
2025-01-09 15:04:04 +0000

    feat: implement dynamic service status updates with randomization logic

--- src/app/dashboard/service-status/service-status.component.ts
+++ src/app/dashboard/service-status/service-status.component.ts
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './service-status.component.css'
 })
 export class ServiceStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.9999999999999999
+
+      if (rnd > 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+  }
 
 }
